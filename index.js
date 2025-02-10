06d709498b3b9a3d613fd0a68b1efd60ceb20681
David Valentino <hi@davidvalentino.com>
2025-02-11 00:57:34 +0700

    feat: add function to scrap logammulia.com

---
+++ index.js
@@ -0,0 +1,39 @@
+import * as dotenv from 'dotenv';
+import cron from 'node-cron';
+import express from 'express';
+import { fetchLogamMulia } from './scrapers/logammulia.js';
+import dayjs from 'dayjs';
+import axios from 'axios';
+
+dotenv.config();
+process.setMaxListeners(20);
+
+// basic setup
+const app = express();
+const port = Number(process.env.PORT) || 5700;
+const isProduction = process.env.NODE_ENV === 'production';
+const cronSchedule = !isProduction ? '*/1 * * * *' : '0 9 * * *';
+
+// telegram credentials
+const token = process.env.TELEGRAM_BOT_TOKEN;
+const channelId = process.env.TELEGRAM_CHANNEL_ID;
+
+cron.schedule(cronSchedule, async () => {
+  if(!isProduction) {
+    console.log(`Running task to scraping gold price list from logammulia.com at ${dayjs().format('DD MMMM YYYY HH:mm:ss')}`);
+  }
+  fetchLogamMulia(token, channelId, isProduction);
+});
+
+app.listen(port, async () => {
+  const message = `Service is running at ${dayjs().format('DD MMMM YYYY HH:mm:ss')}`;
+  console.log(`${message} on port ${port} in ${isProduction ? 'production' : 'development'} mode`);
+  await axios.post(`https://api.telegram.org/bot${token}/sendMessage`, {
+    chat_id: `${channelId}`,
+    text: `${isProduction ? '' : '<b>UJI COBA - HARAP ABAIKAN</b>&#10;&#10;'}${message}`,
+    parse_mode: 'HTML'
+  });
+
+});
+
+process.removeAllListeners();
---
+++ scrapers/logammulia.js
@@ -0,0 +1,51 @@
+// https://logammulia.com
+
+import axios from 'axios';
+import { load } from 'cheerio';
+import puppeteer from "puppeteer";
+import dayjs from 'dayjs';
+
+const URL = "https://www.logammulia.com/id/harga-emas-hari-ini";
+
+export const fetchLogamMulia = async (telegramToken, telegramChannelId, isProductionEnvironment) => {
+  try {
+    const browser = await puppeteer.launch({ headless: "new" });
+    const page = await browser.newPage();
+
+    // Scrap website using puppeteer
+    await page.setUserAgent("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36");
+
+    await page.goto(URL, {
+        waitUntil: "networkidle2"
+    });
+
+    const pageContent = await page.content();
+    await browser.close();
+
+    const goldPrices = [];
+
+    // parsing base on pageContent
+    const $ = load(pageContent);
+    $(".table.table-bordered tbody tr").each((_, row) => {
+      const weight = $(row).find("td:nth-child(1)").text().trim();
+      const price = $(row).find("td:nth-child(2)").text().trim();
+
+      if (weight && price) {
+        goldPrices.push({ weight, price });
+      }
+    });
+
+    // build message to send to telegram
+    const title = `${isProductionEnvironment ? '' : '<b>UJI COBA - HARAP ABAIKAN</b>&#10;&#10;'}Harga Emas Logam Mulia Hari Ini. Hasil scraping dari https://logammulia.com pada ${dayjs().format('DD MMMM YYYY HH:mm:ss')}`;
+    const message = goldPrices.map((item) => `<b>${item.weight}</b> : Rp ${item.price}`).join("&#10;&#10;");
+    const messageToSend = `${title}&#10;&#10;${message}`;
+
+    await axios.post(`https://api.telegram.org/bot${telegramToken}/sendMessage`, {
+      chat_id: `${telegramChannelId}`,
+      text: `${messageToSend}`,
+      parse_mode: 'HTML'
+    });
+  } catch (error) {
+    console.error('An error occurred', error);
+  }
+};
